a41456702fb1eed4b086fcab97dfc29a449e7011
rzkydrmwn <mawand888@gmail.com>
2017-01-02 00:36:06 +0700

    rev 05

--- XREMEDY2/ambil2.sh
+++ XREMEDY2/ambil2.sh
@@ -2,30 +2,30 @@
 # (c) 2016 M. Rizky Darmawan -- This is free software
 # Feel free to copy and/or modify and/or distribute it,
 # provided this notice, and the copyright notice, are preserved.
+# REV02 Sun  1 20:22:31 WIB 2017
 # REV01 Sun  1 11:55:22 WIB 2017
 # START Sat 31 17:22:10 WIB 2016
 # Script menyimpan array name & url, melakukan pengecekan apakah
 # name dapat di dump, bila gagal akan dibuat file kosong RK-XX-GAGAL.txt
 # bila berhasil, hasil dump akan diisikan ke dalam RK-XX.txt
 
-tmp="$(awk '{print $2}' ranking.txt)"
+tmp="$(grep -o 'RK-[0-9][0-9]' ranking.txt)"
 name=()
 for val in $tmp; do
-	name+=("$val")
+        name+=("$val")
 done
-tmp2="$(awk '{print $3}' ranking.txt)"
+
+tmp2="$(grep -o 'http[^ ]*' ranking.txt)"
 url=()
-for val2 in $tmp2; do 
+for val2 in $tmp2; do
 	url+=("$val2")
 done
 
 for ((i=0; i<${#name[@]}; i++)); do
 	successPage="$(w3m -dump ${url[i]})"
-	if [ -z "$successpage" ]; then
+	if [ -z "$successPage" ]; then
 		touch "${name[i]}-GAGAL.txt"
 	else
-	failedPage = "$(w3m -dump -cols 120 ${url[i]})"
-	echo "$failedPage" > "${name[i]}.txt"
+                w3m -dump -cols 2000 ${url[i]} > "${name[i]}.txt"
 	fi
 done
-
